0bc36fe6ab3722e948c8ae86f921e2095b021d70
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-25 17:51:31 +0200

    [UPD] created app.blade.php in layouts folder. Created index.blade.php in movie folder. Created a route called MovieController

---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="movies">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    ciao
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
