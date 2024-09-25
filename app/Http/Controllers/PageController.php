608f819a0edc60a6e53b21e0f8ddb950bd6da28c
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-25 18:26:14 +0200

    [UPD] diplayed movies data in 'dump'

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('movie.index');
+        $movies = Movie::all();
+        dd($movies);
+        return view('movies.index');
     }
 }
